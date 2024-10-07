3ebaef3013a3e863a5b7f052e9801f7da5025275
unknown <nathanmheaton@gmail.com>
2024-10-07 16:04:07 +0100

    added background and code template from previous project

---
+++ assets/file.js
@@ -0,0 +1,207 @@
+// canvas
+//==============================================
+const canvas = document.getElementById("canvas")
+const context = canvas.getContext("2d");
+const contextText = canvas.getContext("2d");
+const CANVAS_WIDTH = 1920;
+const CANVAS_HEIGHT = 1080;
+context.imageSmoothingEnabled =false;
+
+
+//fishing vars
+//======================================
+let hookX = 200;
+let hookY = 250;
+
+let casted = true;
+
+let reelSpeed = 2;
+
+let cooldown = 150;
+
+let victory = false;
+let canInteract = false;
+
+// image setup
+//=======================================================
+let background = new Image();
+background.src = "assets/img/Background + UI elements.png";
+
+
+// audio setup
+//=========================================================
+
+//let music = new Audio("assets/audio/dark castle Am ver.2.mp3");
+
+// GameObject holds positional information
+// Can be used to hold other information based on requirements
+//=======================================================
+function GameObject(spritesheet, x, y, width, height) {
+    this.spritesheet = spritesheet;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+}
+
+// game object setup
+//=======================================================
+//let player = new GameObject(playerImage, playerx, playery, 16, 16);
+
+
+// render
+//=======================================================
+let pause = false;
+let fishing = true;
+let option = false
+
+// window
+//============================================================
+
+const ASPECT_RATIO = 1080/1920;
+let optimalWidthtotal = 1920;
+
+setCanvasSize();
+
+window.addEventListener("resize",setCanvasSize);
+
+function setCanvasSize()
+{
+    optimalWidthtotal = document.getElementsByClassName("container")[0].clientWidth;
+    if (optimalWidthtotal > 1920)
+        {
+            optimalWidthtotal = 1920;
+        }
+    canvas.width = optimalWidthtotal;
+    canvas.height = optimalWidthtotal * ASPECT_RATIO; // calclates the best height to view the content
+}
+
+function draw() {
+    // Clear Canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    
+
+    if (pause == true) // can pause on any screen
+    {
+        context.fillStyle = "#00000088";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        // pause title
+        contextText.fillStyle = "white";
+        contextText.font = "80px Arial";
+        contextText.fillText("Pause", 450, 150);
+
+        contextText.font = "30px Copperplate";
+        contextText.fillText("Movement: WASD \n Interact: e \n Sprint: h", 10, 50);
+        
+        //return to game button
+        context.fillRect(450,200,200,75);
+        contextText.fillStyle = "black";
+        contextText.fillText("Return",500,240);
+        
+        // options button
+        contextText.fillStyle = "white";
+        context.fillRect(450,300,200,75);
+        contextText.fillStyle = "black";
+        contextText.fillText("Options",500,340);
+
+    }
+
+    if (option == true)
+    {
+        context.fillStyle = "#000";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+    }
+
+    if (fishing == true)
+    {
+        context.drawImage(background,0,0,canvas.width,canvas.height);
+    }
+
+}
+
+
+
+function backgroundDrawer()
+{
+    if (timePeriod == enum_time.present)// present floor
+    {
+        context.drawImage(floorPresent,0,0,floorPresent.width,floorPresent.height,0,0,floorPresent.width *4,floorPresent.height*4);
+    }
+}
+const WALL_FRAME_WIDTH = 26;
+const WALL_FRAME_HEIGHT = 67;
+
+
+let musicActive = false;
+function playBackgroundMusic()
+{
+    
+    if (musicActive == false)
+    {
+        music.play();
+        musicActive = true;
+    }
+    
+}
+
+function changeVolume()
+{
+
+
+}
+
+function update() {
+    
+
+    if (pause == true )
+    {
+
+    }
+    else if (option)
+    {
+        
+    }
+    else
+    {
+
+    }
+}
+
+function gameloop() {
+    update();
+    draw();
+    window.requestAnimationFrame(gameloop);
+}
+
+function boundaryCheck(){
+
+}
+
+
+let respawnTime = 300;
+let respawnActive = false;
+
+function resetGame()
+{
+
+}
+
+function winGame()
+{
+
+}
+
+
+// Handle Active Browser Tag Animation
+window.requestAnimationFrame(gameloop);
+
+// https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+
+window.addEventListener('keydown', input);
+// disable the second event listener if you want continuous movement
+window.addEventListener('keyup', input);
+
+window.addEventListener("mousedown", input)
---
+++ assets/inputHandler.js
@@ -0,0 +1,181 @@
+// player input
+//=======================================================
+function GamerInput(input) {
+    this.action = input; // Hold the current input as a string
+}
+
+// Default GamerInput is set to None
+let gamerInput = new GamerInput("None"); //No Input
+
+// mouse and touch
+
+let mousePos = {
+    x: 0,
+    y: 0,
+}
+
+window.addEventListener("touchstart", touchInput);
+
+
+canvas.addEventListener("click", mouseInput)
+
+let isDragging = false;
+
+function mouseInput(event)
+{
+    // Get the mouse coordinates relative to the canvas
+    var mouseX = event.clientX - canvas.getBoundingClientRect().left;
+    var mouseY = event.clientY - canvas.getBoundingClientRect().top;
+
+    if(pause)// when in the pause screen
+    {
+        // Check if the click is inside the box
+        if (mouseX >= 450 && mouseX <= 600 && mouseY >= 200 && mouseY <= 275) 
+        {
+            pause = false;
+        }
+
+        if (mouseX >= 450 && mouseX <= 600 && mouseY >= 300 && mouseY <= 375) 
+        {
+            option = true;
+        }
+    }
+
+    if(option)
+    {
+        // Check if the click is inside the box
+        if (mouseX >= 450 && mouseX <= 600 && mouseY >= 200 && mouseY <= 275) 
+        {
+            pause = false;
+            option = false;
+        }
+
+        if (mouseX >= 450 && mouseX <= 600 && mouseY >= 300 && mouseY <= 375) 
+        {
+            audioMute = !audioMute;
+            changeVolume();
+        }
+
+        // for music volume slider
+        if (mouseX >= 100 && mouseX <= 300 && mouseY >= 200 && mouseY <= 225) 
+        {
+            isDragging = true;
+            
+            handleMouseMoveMusic(event);
+            changeVolume();
+
+        }
+        //for
+        if (mouseX >= 100 && mouseX <= 300 && mouseY >= 300 && mouseY <= 325) 
+        {
+            isDragging = true;
+            handleMouseMoveSFX(event);
+            changeVolume();
+        }
+
+
+    }
+}
+
+function pauseToggle()
+{
+    pause = !pause;
+}
+
+function touchInput(event)
+{
+    // Get the touch coordinates relative to the canvas
+    var touchX = event.clientX - canvas.getBoundingClientRect().left;
+    var touchY = event.clientY - canvas.getBoundingClientRect().top;
+
+    
+    if(pause)// when in the pause screen
+    {
+        // Check if the click is inside the box
+        if (touchX >= 450 && touchX <= 600 && touchY >= 200 && touchY <= 275) 
+        {
+            pause = false;
+        }
+
+        if (touchX >= 450 && touchX <= 600 && touchY >= 300 && touchY <= 375) 
+        {
+            option = true;
+        }
+    }
+    else{
+        if (touchX >= 0 && touchX <= 50 && touchY >= 0 && touchY <= 50) 
+        {
+            pause = true;
+        }
+
+    }
+
+    if(option)
+    {
+        // Check if the click is inside the box
+        if (touchX >= 450 && touchX <= 600 && touchY >= 200 && touchY <= 275) 
+        {
+            pause = false;
+            option = false;
+        }
+        if (touchX >= 450 && touchX <= 600 && touchY >= 300 && touchY <= 375) 
+        {
+            audioMute = !audioMute;
+            changeVolume();
+        }
+    }
+}
+
+let sprintState = 0; // bool for sprinting
+function input(event) {
+    // Take Input from the Player
+    // console.log("Input");
+    console.log(event);
+    console.log("Event type: " + event.type);
+    // console.log("Keycode: " + event.key);
+if (event.type === "keydown") {
+    switch (event.key) {
+        case "a": // Left Arrow
+            gamerInput = new GamerInput("Left");
+            blueButton.classList.add("pressed");
+            break; //Left key
+        case "w": // Up Arrow
+            gamerInput = new GamerInput("Up");
+            yellowButton.classList.add("pressed");
+            break; //Up key
+        case "d": // Right Arrow
+            gamerInput = new GamerInput("Right");
+            redButton.classList.add("pressed");
+            break; //Right key
+        case "s": // Down Arrow
+            gamerInput = new GamerInput("Down");
+            greenButton.classList.add("pressed");
+            break; //h key
+        case "h":// sprint
+            if (!cooldownState) // if there is no cooldown on
+            {
+            sprintState = true;
+            }
+            sprintButton.classList.add("pressed");
+            break; //Down key
+        case "e": // e key;
+            interactButton.classList.add("pressed");
+            if (canInteract == true)
+            {
+                switchState = !switchState ;// means it is toogled
+                console.log("toggled");
+            }
+            break;
+        case "Escape":
+            pause = !pause;// toggles pause
+            console.log(pause);
+            break;
+        case "1":
+            break;
+
+
+        default:
+            gamerInput = new GamerInput("None"); //No Input
+            sprintState = false;
+    }
+}}
